aa4977e8b3acc960739ec0e338adda466a131162
Fredrik Noren <noren@spotify.com>
2015-08-03 22:51:33 -0400

    Deploy bugfix

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-docker rm deepkeep-package-repository
 docker stop deepkeep-package-repository
+docker rm deepkeep-package-repository
 git pull
 docker build -t deepkeep-package-repository .
 docker run -p 80:80 --env-file .env -d --name deepkeep-package-repository deepkeep-package-repository
